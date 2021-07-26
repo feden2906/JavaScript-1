5b120260faa7fed36b5cd4ebed195182051dab0a
zemferonok <zemferonok@gmail.com>
2021-07-26 15:12:57 +0300

    add: Lesson_3 ClassWork Lesson_4 HomeWork Lesson_4 ClassWork

--- lesson_3/HomeWork/HomeWork_3.js
+++ lesson_3/HomeWork/HomeWork_3.js
@@ -2,35 +2,42 @@
 function SquareRectangle(side_a, side_b) {
     return side_a * side_b;
 }
-document.write(`<div>SquareRectangle (4, 5) = ${SquareRectangle(4, 5)}</div>`)  // результат виконання записуй в змінну потім використовуй його
+
+let squareRectangle = SquareRectangle(4, 5);
+document.write(`<div>SquareRectangle (4, 5) = ${squareRectangle}</div>`)
+
 
 
 // - створити функцію яка обчислює та повертає площу кола
 function SquareRound(radius) {
     // let Pi = 3.14;
-    return Math.PI * Math.pow(radius, 2)             // Math.PI використовуй замість 3.14
+    return Math.PI * Math.pow(radius, 2)
 }
-document.write(`<div>SquareRound(6) = ${SquareRound(6)}</div>`);
+
+let squareRound = SquareRound(6);
+document.write(`<div>SquareRound(6) = ${squareRound}</div>`);
+
 
 
 // - створити функцію яка обчислює та повертає площу циліндру
 let SquareCylinder = function (radius, height) {
     let lengthRound = 2 * 3.14 * radius;
-    // return SquareRound(radius) + SquareRectangle(lengthRound, height);  // как сократить точность до 2 знаков после запятой??
-    return (SquareRound(radius) + SquareRectangle(lengthRound, height)).toFixed(2);     // так
+    // return SquareRound(radius) + SquareRectangle(lengthRound, height);  //точность 2 знака после запятой??//
+    return SquareRound(radius) + SquareRectangle(lengthRound, height).toFixed(2);
 }
-document.write(`<div>SquareCylinder(6, 10) = ${SquareCylinder(6, 10)}</div>`);
+
+let squareCylinder = SquareCylinder(6, 10);
+document.write(`<div>SquareCylinder(6, 10) = ${squareCylinder}</div>`);
+
 
 
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
-let minMaxNumbers = function minMaxNumbers() {
+let numbers = [3, 4, 2, 5, 1, 69, 9, 8, 7, 0, -100];
+
+function minMaxNumbers(arguments) {
     let min = arguments[0];
     let max = arguments[0];
     for (let argument of arguments) {
-        // if (!min && !max) {                      // ------
-        //     min = argument;                      // це можна забрати якщо запишеш перше значення в змінну
-        //     max = argument;                      //
-        // }                                        // ------
         if (min > argument) {
             min = argument;
         }
@@ -40,23 +47,23 @@ let minMaxNumbers = function minMaxNumbers() {
     }
     console.log(`Max is ${max}`)
     return min;
-};
-console.log(minMaxNumbers(3,4,2,5,1,69,9,8,7,0,-100));
+}
+
+console.log(minMaxNumbers(numbers));
+
 
 
 // - створити функцію яка  створює блок з текстом. Текст задати через аргумент
-function textMaker (content) {
-    document.write(`
-    <div>
-        <p>${content}</p>
-    </div>
-    `);
+function textMaker(content) {
+    document.write(`<div><p>${content}</p></div>`);
 }
+
 textMaker("write some text");
 
 
+
 // - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-function ulMaker (content) {
+function ulMaker(content) {
     document.write(`
     <ul>
         <li>${content}</li>
@@ -65,29 +72,34 @@ function ulMaker (content) {
     </ul>
     `);
 }
+
 ulMaker("Text for Ul");
+
 
 
 // - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
 // Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-function ulFuncMaker (content, count) {
-    document.write("<ul>");
+function ulFuncMaker(content, count) {
+    document.write("<ul>");                     //Тут нужны двойные кавычки?
     for (let i = 0; i < count; i++) {
         document.write(`<li>${content}</li>`);
     }
     document.write("</ul>");
 }
+
 ulFuncMaker("Text for funcUl", 3);
 
 
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 let primArray = [100500, "Go and be drunk", true, 22, false];
-function primArrayList (array) {
+
+function primArrayList(array) {
     document.write("<ul>");
     for (let arrayElement of array) {
         document.write(`<li>${arrayElement}</li>`);
     }
     document.write("</ul>");
 }
+
 primArrayList(primArray);
