0fb2b9490524058279e793d986248f119b5ce519
zemferonok <zemferonok@gmail.com>
2021-07-28 15:39:59 +0300

    HomeWork_5 is Complete

--- lesson_5/HomeWork/HomeWork_5.js
+++ lesson_5/HomeWork/HomeWork_5.js
@@ -6,9 +6,11 @@ let contentText = document.getElementById('content');
 console.log(contentText.innerText);
 
 
+
 // -- отримує текст з блоку з id "rules"
 let rulesText = document.getElementById('rules').innerText;
 console.log(rulesText);
+
 
 
 // -- замініть текст параграфа з id 'content' на будь-який інший
@@ -18,8 +20,56 @@ console.log(contentText.innerText);
 
 
 // -- замініть текст параграфа з id 'rules' на будь-який інший
+document.getElementById('rules').innerText = `Any ander text, new rules`;
+
+
+
 // -- змініть кожному елементу колір фону на червоний
+let elements = document.body.children;
+
+for (let element of elements) {
+    element.style.backgroundColor = 'pink';
+}
+// let elements = document.querySelectorAll('body > *');        //Новый способ выбора елементов..
+// console.log(elements);
+// for (let element of elements) {
+//     element.style.backgroundColor = 'red';
+// }
+
+// let bodyElements = document.getElementsByTagName(`*`);           // Докапывается абсолютно до всех Тегов
+
+// let bodyElements = document.body.getElementsByTagName(`*`);      // Докапывается до всех Тегов в Боди
+// for (let bodyElement of bodyElements) {
+//     bodyElement.style.backgroundColor = 'gray';
+// }
+
+
+
 // -- змініть кожному елементу колір тексту на синій
+for (let element of elements) {
+    element.style.color = 'blue';
+}
+
+
+
 // -- отримати весь список класів елемента з id=rules і вивести їх в console.log
-// -- отримати всі елементи з класом fc_rules. визначити їм 2 події "клік", одна вивдоить текст елементу, інша довільний ьексь
+console.log('List of class by id: rules, moving in array.',[...document.getElementById('rules').classList]);
+
+
+
+// -- отримати всі елементи з класом fc_rules. визначити їм 2 події "клік", одна вивдоить текст елементу, інша довільний text
+let fc_rulesElements = document.getElementsByClassName('fc_rules');
+
+for (let fc_RulesElement of fc_rulesElements) {
+    fc_RulesElement.onclick = function () {
+        console.log(fc_RulesElement.innerText);
+        console.log('Any ander text..');
+    };
+}
+
+
+
 // -- поміняти колір тексту у всіх елементів fc_rules на червоний
+for (let fc_RulesElement of fc_rulesElements) {
+    fc_RulesElement.style.color = 'red';
+}
