56905ef3927306caf4cb668d0f785e9a69efea78
zemferonok <zemferonok@gmail.com>
2021-08-08 23:09:45 +0300

    Project add user_details

--- Project/user_details.js
+++ Project/user_details.js
@@ -9,6 +9,32 @@ userDiv.classList.add('userInfo');
 
 fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
     .then(value => value.json())
-    .then(user => {console.log(user)})
+    .then(user => {
+        console.log(user);
+        // userDiv.innerText = JSON.stringify(user);    // Delete this string of code
+        let infoList = document.createElement('ul');
+
+        for (let key in user) {
+            let object = typeof user[key] === 'object';
+            if (object) {
+                let ul = document.createElement('ul');
+
+                for (let value in user[key]) {
+                    let stripUl = document.createElement('li');
+                    stripUl.innerText = `${value} : ${user[key][value]} (${typeof user[key][value]})`;
+                    ul.appendChild(stripUl);
+                }
+
+                infoList.appendChild(ul);
+            } else {
+                let strip = document.createElement('li');
+                strip.innerText = `${key} : ${user[key]} (${typeof user[key]})`;
+                infoList.appendChild(strip);
+            }
+        }
+
+        userDiv.appendChild(infoList);
+    })
+    // Как?     Добавить обработку ошибки. Если ссылка не ликвидна, дать сообщение об этом.
 
 document.body.appendChild(userDiv);
